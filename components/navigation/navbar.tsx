70bac1dab0f1dab38c5e2e517abc0feb80b96440
Sim Jing Jia <simjingjia2016@gmail.com>
2025-01-29 02:24:18 +0800

    basic setup

---
+++ components/navigation/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="py-4 border-b">
+       <div className="md:w-[95%]">
+
+       </div>
+    </nav>
+  )
+}
+
+export default Navbar;
